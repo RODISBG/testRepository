676506cad10729fca207e7a2cf9f1341257ff6ae
RODISBG <85491110+RODISBG@users.noreply.github.com>
2023-08-08 16:23:23 +0300

    Update gitHubTest.js

--- gitHubTest.js
+++ gitHubTest.js
@@ -1,10 +1,10 @@
 function dog() {
     let dog = 'artemida';
     let age = 9;
-    console.log(`${dog},${age}`);
     let dogNew = 'charlie';
     let dogNewAge = 10;
     console.log(`${dogNew},${dogNewAge}`);
+    console.log(`${dog},${age}`);
 
     
 }
